1e3ad24ecf541c358dc530730fb1580e6cd639d5
duyhieu <duyhieu@06423ec0-a894-4cfd-a1c2-12d213ee8c5f>
2017-12-08 01:31:32 +0000

    - Removing ieee.fixed_float_types because this one is ont used and it blocks synthesis
    git-svn-id: http://10.10.1.61/repos/Giants/branches/lsi_contest_2018@61 06423ec0-a894-4cfd-a1c2-12d213ee8c5f

--- src/rtl/rtl_pkg.vhd
+++ src/rtl/rtl_pkg.vhd
@@ -24,7 +24,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.fixed_pkg.all;
-use ieee.fixed_float_types.all;
 use ieee.numeric_std.all;
 
 ---------------------------------------------------------------------------------
